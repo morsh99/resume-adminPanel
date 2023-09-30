ff657b71d55d60f52c096bb6eb7d605f4ba15eaf
Morsh <sharifi.morsh@gmail.com>
2023-09-30 13:11:17 +0330

    initial commit

---
+++ resumePresentation/Controllers/About.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace resumePresentation.Controllers
+{
+	public class About : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Salam()
+		{
+			return View();
+		}
+	}
+}
